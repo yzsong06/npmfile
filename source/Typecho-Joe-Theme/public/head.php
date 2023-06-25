7c45537a90c0c162587cdabd8fca3de92ec46e51
一只鬆 <yzsong06@outlook.com>
2023-06-25 20:04:08 +0800

    Update head.php

--- source/Typecho-Joe-Theme/public/head.php
+++ source/Typecho-Joe-Theme/public/head.php
@@ -77,7 +77,7 @@
 
 <!-- 代码高亮 -->
 <?php if ($this->options->JCodeColor !== 'off') : ?>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.2.1/build/styles/<?php $this->options->JCodeColor() ?>.min.css">
+    <link rel="stylesheet" href="https://npm.elemecdn.com/highlight.js@11.8.0/styles/<?php $this->options->JCodeColor() ?>.css">
 <?php endif; ?>
 
 <!-- 页面加载 -->
